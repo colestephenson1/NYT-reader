355271e2befba037b8d2619ec33ca549a8cce1aa
Cole Stephenson <colestephenson94@gmail.com>
2023-01-04 22:36:01 -0700

    Added 4th article to chosen topic

--- pages/articles/[topic].js
+++ pages/articles/[topic].js
@@ -5,7 +5,7 @@ import styles from '../../styles/HomeArticles.module.css'
 
 const ChosenTopicList = ({articles}) => {
     let count = 0
-    const mappedArticles = articles.filter(article => article.multimedia && article.title).slice(0, 3).map(article => {
+    const mappedArticles = articles.filter(article => article.multimedia && article.title).slice(0, 5).map(article => {
         count++
         const {title, byline, multimedia, url} = article
         return (
@@ -24,7 +24,8 @@ const ChosenTopicList = ({articles}) => {
         <div className={styles.bigBox}>
           <div className={styles.first}>{mappedArticles[0]}</div>
           <div className={styles.second}>{mappedArticles[1]}</div>
-          <div className={styles.third}>{mappedArticles[2]}</div>        
+          <div className={styles.third}>{mappedArticles[2]}</div>
+          <div className={styles.fourth}>{mappedArticles[3]}</div>           
         </div>
       </Layout>
     </div>
