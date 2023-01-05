8197250c6fecc1183bf5ec9af54801bb6b72e0ca
Cole Stephenson <colestephenson94@gmail.com>
2023-01-04 22:07:26 -0700

    Implement dynamic routing for topic choice

--- Components/NavBar/NavBar.js
+++ Components/NavBar/NavBar.js
@@ -10,11 +10,11 @@ const NavBar = () => {
         <div id="navbarMenu" className="navbar-menu">
             <Link legacyBehavior href="/"><a className="navbar-item is-active is-size-5 has-text-weight-bold">Home </a></Link>
             <div className="navbar-end">
-                <Link legacyBehavior href="/"><a className="navbar-item is-active is-size-5 has-text-weight-semibold">Arts</a></Link>
-                <Link legacyBehavior href="/"><a className="navbar-item is-active is-size-5 has-text-weight-semibold">Sports</a></Link>
-                <Link legacyBehavior href="/"><a className="navbar-item is-active is-size-5 has-text-weight-semibold">Movies</a></Link>
-                <Link legacyBehavior href="/"><a className="navbar-item is-active is-size-5 has-text-weight-semibold"> U.S</a></Link>
-                <Link legacyBehavior href="/"><a className="navbar-item is-active is-size-5 has-text-weight-semibold">World</a></Link>
+                <Link legacyBehavior href="/articles/arts"><a className="navbar-item is-active is-size-5 has-text-weight-semibold">Arts</a></Link>
+                <Link legacyBehavior href="/articles/sports"><a className="navbar-item is-active is-size-5 has-text-weight-semibold">Sports</a></Link>
+                <Link legacyBehavior href="/articles/movies"><a className="navbar-item is-active is-size-5 has-text-weight-semibold">Movies</a></Link>
+                <Link legacyBehavior href="/articles/us"><a className="navbar-item is-active is-size-5 has-text-weight-semibold"> U.S</a></Link>
+                <Link legacyBehavior href="/articles/world"><a className="navbar-item is-active is-size-5 has-text-weight-semibold">World</a></Link>
             </div>
         </div>
         </div>
---
+++ pages/articles/[topic].js
@@ -0,0 +1,46 @@
+import React from 'react'
+import SingleArticle from '../../Components/SingleArticle/SingleArticle'
+import Layout from '../../Components/Layout/Layout'
+import styles from '../../styles/HomeArticles.module.css'
+
+const ChosenTopicList = ({articles}) => {
+    let count = 0
+    const mappedArticles = articles.filter(article => article.multimedia && article.title).slice(0, 3).map(article => {
+        count++
+        const {title, byline, multimedia, url} = article
+        return (
+            <SingleArticle
+                key={count}
+                title={title}
+                byline={byline}
+                image={multimedia[0].url}
+                url={url}/>
+         )
+    })
+
+  return (
+    <div>
+      <Layout>
+        <div className={styles.bigBox}>
+          <div className={styles.first}>{mappedArticles[0]}</div>
+          <div className={styles.second}>{mappedArticles[1]}</div>
+          <div className={styles.third}>{mappedArticles[2]}</div>        
+        </div>
+      </Layout>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context) {
+    const topic = context.params.topic
+    const resData = await fetch(`https://api.nytimes.com/svc/topstories/v2/${topic}.json?api-key=YMwGtcuTdYGINAxVqRw5puQauT6dAhD3`)
+    const data = await resData.json();
+
+    return {
+      props: {
+        articles: data.results
+      }
+    }
+}
+
+export default ChosenTopicList
